b68ef87b01967d199aec8e40dabfd1d04d26466a
Nazaziik <nazar240902@gmail.com>
2023-12-06 20:55:26 -0600

    Added Dependency Injections from Infrastructure and Presentation

---
+++ Identity_Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Identity_Application.Interfaces;
+using Identity_Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity_Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+}
--- Identity_Presentation/Program.cs
+++ Identity_Presentation/Program.cs
@@ -1,8 +1,12 @@
+using Identity_Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
